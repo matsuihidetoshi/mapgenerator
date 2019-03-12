e2800cd6ab77805177a4df99f495e1381d6098b5
matsuihidetoshi <partyrockeverydayimshufflin@gmail.com>
2019-03-13 08:08:33 +0900

    add edit button on map_former

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -4,24 +4,24 @@ class PartsController < ApplicationController
   require "fileutils"
 
   def new
-    @parent = Part.find(params[:id])
+    @parent = current_user.parts.find(params[:id])
     @part = Part.new
     if params[:edit].present?
       @back_to = 'edit'
     elsif params[:map_id].present?
-      @back_to = Part.find(params[:map_id])
+      @back_to = current_user.parts.find(params[:map_id])
     end
   end
 
   def create
-    @parent = Part.find(params[:part][:parent_id])
+    @parent = current_user.parts.find(params[:part][:parent_id])
     @part = current_user.parts.build(part_params)
     if @part.save
       flash[:success] = 'Created a part'
       @parent.relate(@part)
       @parent.save
       if params[:part][:map_id]
-        redirect_to map_former_path(Part.find(params[:part][:map_id]))
+        redirect_to map_former_path(current_user.parts.find(params[:part][:map_id]))
       else
         redirect_to root_path
       end
@@ -33,15 +33,23 @@ class PartsController < ApplicationController
   end
 
   def edit
-    @part = current_user.parts.find(params[:id])
+    if params[:map_id]
+      @part = current_user.parts.find(params[:edit_id])
+      @back_to = current_user.parts.find(params[:map_id])
+    else
+      @part = current_user.parts.find(params[:id])
+    end
   end
 
   def update
     @part = current_user.parts.find(params[:id])
-
     if @part.update(part_params)
       flash[:success] = 'Updated the part'
-      redirect_to root_path
+      if params[:part][:map_id]
+        redirect_to map_former_path(current_user.parts.find(params[:part][:map_id]))
+      else
+        redirect_to root_path
+      end
     else
       flash.now[:danger] = 'Failed to update the part'
       render :edit
@@ -50,13 +58,13 @@ class PartsController < ApplicationController
 
   def destroy
     if params[:map_id].present?
-      @part = Part.find(params[:delete_id])
+      @part = current_user.parts.find(params[:delete_id])
     else
-      @part = Part.find(params[:id])
+      @part = current_user.parts.find(params[:id])
     end
     @part.destroy
     flash[:success] = 'Removed the part'
-    if params[:map_id].present? && (part = Part.find(params[:map_id])).relatings.any?
+    if params[:map_id].present? && (part = current_user.parts.find(params[:map_id])).relatings.any?
       redirect_to map_former_path(part)
     else
       redirect_to root_path
